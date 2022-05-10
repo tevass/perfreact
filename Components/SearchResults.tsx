5f2c54012bc34b76eaf59be9bc350d388ae9d884
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2022-05-10 20:51:35 -0300

    Virtualização

--- Components/SearchResults.tsx
+++ Components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { List, ListRowRenderer } from 'react-virtualized'
+
 import { ProductItem } from "./ProdcuctIem";
 
 type SearchResultProps = {
@@ -12,19 +14,29 @@ type SearchResultProps = {
 }
 
 export function SearchResult({ results, onAddToWishList, totalPrice }: SearchResultProps) {
+  const rowRender: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  }
+  
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      { results.map(product => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishList={onAddToWishList}
-          />
-        )
-      }) }
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRender}
+      />
 
     </div>
   )
