a858dc36c4e57444078213f8cb0d7943f5acb8ee
Estevão Gabriel Biondi <teh.gabriel10@gmail.com>
2022-05-11 20:32:05 -0300

    Bundle analyser / Performando apps com ReactJS

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return config
+  },
+}
